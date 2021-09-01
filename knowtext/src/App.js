abc0a4a0e5977affaf89f16d1727cebcdb65f68e
krishnapal singh rawat <47602022+krishkprawat@users.noreply.github.com>
2021-09-01 12:48:17 +0530

    dark and ight mode

--- knowtext/src/App.js
+++ knowtext/src/App.js
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
-import Textform from "./components/Textform";
+//import Textform from "./components/Textform";
 
+import About from "./components/about";
 function App() {
   return (
     <>
       <Navbar title="Know-your-Text" />
       <div className="container my-3">
-        <Textform heading="Enter your text" />
+        <About />
       </div>
     </>
   );
